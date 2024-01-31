f684295def120ddf3c897e507c2fb82ac3332b5e
KimWang906 <hyunbin06git@gmail.com>
2024-01-31 19:02:44 +0900

    ADD: pwn env, example env

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "PWNABLE Shell env";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: 
+  let 
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in
+  {
+    devShells.${system}.default = 
+      pkgs.mkShell.override { stdenv = pkgs.multiStdenv; } {
+          buildInputs = with pkgs; [
+            pwndbg
+            tmux
+            pwntools
+            checksec
+            ropgadget
+            one_gadget
+          ];
+
+          shellHook = ''
+            cp config/.tmux.conf $HOME
+            tmux
+            alias gdb="pwndbg"
+          '';
+        };
+  };
+}
